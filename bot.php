232995220cf3738ae237140dcf463bdfca54f6b6
jay <jr30149_@hotmail.com>
2016-12-11 22:32:51 +0700

    dddddd

--- bot.php
+++ bot.php
@@ -203,19 +203,22 @@ if (!is_null($events['events'])) {
 									'replyToken' => $replyToken,
 									'messages' => [$messages3]
 								];
-								$post = json_encode($data);
-								$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-					
-								$ch = curl_init($url);
-								curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-								curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-								curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-								curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-								$result = curl_exec($ch);
-								curl_close($ch);
 								
+								if($hoonname=="checkgpu")
+								{
+									$post = json_encode($data);
+									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						
+									$ch = curl_init($url);
+									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									$result = curl_exec($ch);
+									curl_close($ch);
 								
+								}
 								
 								
 								
