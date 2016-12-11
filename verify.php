c1695738fd81ed424b1193bed5e7ba5a435e4d65
jay <jr30149_@hotmail.com>
2016-12-11 22:27:32 +0700

    dddd

--- verify.php
+++ verify.php
@@ -1,12 +1,35 @@
 ﻿<?php
-// page2.php
 
-session_start();
+$access_token = 'J81JqjhEqIJMF6okb9jTNt3HWNZiE1zhZrmhbS3WZ/KTQovd6HzM5B+iQYnlFt7wz1S+UJyFzmGnd/AivBF0v1Lz9jaKAnxNsWxBlLSNWmkn1otG8tlzONrbEx5BOXysEwFX46Zk/AmD7JyKsxy3EQdB04t89/1O/w1cDnyilFU=';
 
+// Get POST body content
+$content = file_get_contents('php://input');
 
+echo "aaaaa";
 
-#echo $_SESSION['favcolor']; // green
-if($_SESSION['favcolor'] == "green")
-	echo "check"
-
+// Parse JSON
+$events = json_decode($content, true);
+				$msg = "adjust gpu temp check";
+				$USERID = "Ub5f45b12f0f8f8a3a08e5b52ebbcc96b";
+				$format_text = [
+					"type" => "text",
+					"text" => $msg
+				];
+		 
+				$post_data = [
+					"to" => $USERID,
+					"messages" => [$format_text]
+				];
+				
+				$header = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		 
+				$ch = curl_init('https://api.line.me/v2/bot/message/push');
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		 
+				$result = curl_exec($ch);
+				curl_close($ch);
 ?>
