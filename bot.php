ef4d384d0b7f8a468e4430b2348b6a04991e2549
jay <jr30149_@hotmail.com>
2016-12-19 23:25:47 +0700

    com10

--- bot.php
+++ bot.php
@@ -126,7 +126,7 @@ if (!is_null($events['events'])) {
 								$room='1';
 								$timeframe = strtolower($timeframe2);
 								sleep(0.1);
-								if($timeframe !="assets" and $timeframe !="libilities" and $timeframe !="equity" and $timeframe !="revenue" and $timeframe !="eps"and $timeframe !="expenses" and $timeframe !="gprofit" and $timeframe !="gmargin" and $timeframe !="nprofit" and $timeframe !="nmp" and $timeframe !="as" and $timeframe !="lb" and $timeframe !="eq" and $timeframe !="rv" and $timeframe !="ex" and $timeframe !="gp" and $timeframe !="gm" and $timeframe !="np" )
+								if($timeframe !="assets" and $timeframe !="liabilities" and $timeframe !="equity" and $timeframe !="revenue" and $timeframe !="eps"and $timeframe !="expenses" and $timeframe !="gprofit" and $timeframe !="gmargin" and $timeframe !="nprofit" and $timeframe !="nmp" and $timeframe !="as" and $timeframe !="lb" and $timeframe !="eq" and $timeframe !="rv" and $timeframe !="ex" and $timeframe !="gp" and $timeframe !="gm" and $timeframe !="np" )
 								{
 									$messages3 = ['type' => 'text','text' => "คำสั่งทั้งหมด             @hoon assets/as       @hoon libilities/lb        @hoon equity/eq        @hoon revenue/rv       @hoon eps                  @hoon expenses/ex       @hoon gprofit/gp        @hoon gmargin/gm        @hoon nprofit/np           @hoon nmp     
 									
@@ -160,7 +160,7 @@ EX   @hoon assert/as                                      ----> @aot assets     
 										$timeframe ="assets";
 									}
 									else if($timeframe =="lb")
-										$timeframe ="libilities";
+										$timeframe ="liabilities";
 									else if($timeframe =="eq")
 										$timeframe ="equity";
 									else if($timeframe =="rv")
