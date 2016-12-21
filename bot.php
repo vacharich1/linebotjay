ed0c30acdbfba66c8ffc1a3417145513d7aa58c7
jay <jr30149_@hotmail.com>
2016-12-21 22:05:50 +0700

    redo

--- bot.php
+++ bot.php
@@ -248,8 +248,8 @@ EX   @hoon assert/as                                      ----> @aot assets     
 									$messages1 = ['type' => 'text','text' => "Please wait 8s"];
 									sleep(1);
 									$messages2 = ['type' => 'image',
-											 'originalContentUrl' => 'https://www.botbottest.club/testtest.jpg',
-											 'previewImageUrl' => 'https://www.botbottest.club/testtest.jpg'
+											 'originalContentUrl' => $link_pic,
+											 'previewImageUrl' => $link_pic
 									];
 									
 									// Make a POST Request to Messaging API to reply to sender
