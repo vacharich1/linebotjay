3bb6e747b1ec70dc48fea45edc2550f65755d654
jay <jr30149_@hotmail.com>
2016-12-23 13:16:23 +0700

    vvvvv

--- bot.php
+++ bot.php
@@ -286,9 +286,9 @@ EX   @hoon assert/as                                      ----> @aot assets     
 						{
 							if(preg_match("/^[a-zA-Z]+$/", $text[0]) == 1) {
 								$replyToken = $event['replyToken'];
-								$messages55 = ['type' => 'image',
-											 'originalContentUrl' => 'https://www.botbottest.club/testtest.jpg',
-											 'previewImageUrl' => 'https://www.botbottest.club/testtest.jpg'
+								$messages55 = ['type' => 'text','text' => "ลืม @ นะจ๊ะ "];
+								// Make a POST Request to Messaging API to reply to sender
+								$url = 'https://api.line.me/v2/bot/message/reply';
 								$data = [
 									'replyToken' => $replyToken,
 									'messages' => [$messages55]
@@ -309,14 +309,21 @@ EX   @hoon assert/as                                      ----> @aot assets     
 							{
 								if($text == 'ทดสอบ' )
 								{
+									
+									$messages55 = ['type' => 'image',
+											 'originalContentUrl' => 'https://www.botbottest.club/testtest.jpg',
+											 'previewImageUrl' => 'https://www.botbottest.club/testtest.jpg'
+											 
+									$replyToken = $event['replyToken'];
+									// Make a POST Request to Messaging API to reply to sender
 									$url = 'https://api.line.me/v2/bot/message/reply';
 									$data = [
-												'replyToken' => $replyToken,
-												'messages' => [$messages55]
-											];
+										'replyToken' => $replyToken,
+										'messages' => [$messages55]
+									];
 									$post = json_encode($data);
 									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-											
+						
 									$ch = curl_init($url);
 									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -325,7 +332,6 @@ EX   @hoon assert/as                                      ----> @aot assets     
 									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 									$result = curl_exec($ch);
 									curl_close($ch);
-									
 								}
 								if($text == 'เเสดงคำสั่งทั้งหมด' || $text == 'คำสั่ง' )
 								{
