76f1df980a5a7e41aa006b75e05777d6370577e7
jay <jr30149_@hotmail.com>
2016-12-23 13:26:12 +0700

    aaaaa

--- bot.php
+++ bot.php
@@ -336,6 +336,34 @@ EX   @hoon assert/as                                      ----> @aot assets     
 									curl_close($ch);
 								}	
 								
+								if($text == 'ทดสอบ')
+								{
+									
+									$replyToken = $event['replyToken'];
+									$messages55 = ['type' => 'image',
+											 'originalContentUrl' => 'https://www.botbottest.club/testtest.jpg',
+											 'previewImageUrl' => 'https://www.botbottest.club/testtest.jpg'];
+											 
+
+									// Make a POST Request to Messaging API to reply to sender
+									$url = 'https://api.line.me/v2/bot/message/reply';
+									$data = [
+										'replyToken' => $replyToken,
+										'messages' => [$messages55]
+									];
+									$post = json_encode($data);
+									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						
+									$ch = curl_init($url);
+									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									$result = curl_exec($ch);
+									curl_close($ch);
+								}
+								
 							}
 							
 						}
