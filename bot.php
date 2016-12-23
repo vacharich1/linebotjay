d02ccc9858b1b650dd216f003f1146f81e56c6a9
jay <jr30149_@hotmail.com>
2016-12-23 13:23:26 +0700

    sssss

--- bot.php
+++ bot.php
@@ -307,31 +307,7 @@ EX   @hoon assert/as                                      ----> @aot assets     
 							}
 							else
 							{
-								if($text == 'ทดสอบ' )
-								{
-									$messages55 = ['type' => 'image',
-											 'originalContentUrl' => 'https://www.botbottest.club/testtest.jpg',
-											 'previewImageUrl' => 'https://www.botbottest.club/testtest.jpg'
-											 
-									$replyToken = $event['replyToken'];
-									// Make a POST Request to Messaging API to reply to sender
-									$url = 'https://api.line.me/v2/bot/message/reply';
-									$data = [
-										'replyToken' => $replyToken,
-										'messages' => [$messages55]
-									];
-									$post = json_encode($data);
-									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-						
-									$ch = curl_init($url);
-									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-									$result = curl_exec($ch);
-									curl_close($ch);
-								}
+								
 								
 								if($text == 'เเสดงคำสั่งทั้งหมด' || $text == 'คำสั่ง' )
 								{
