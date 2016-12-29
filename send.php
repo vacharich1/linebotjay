38555aa4f4d45d60e874fb7cd9b8e6ee6f5089a9
jay <jr30149_@hotmail.com>
2016-12-29 18:35:36 +0700

    sdfsd

--- send.php
+++ send.php
@@ -67,7 +67,7 @@ if ($result->num_rows > 0) {
 		}
 		else if($row["type"]=='hoonsame')
 		{
-			$text_alert = "มีการเเจ้งเตือน".$hoonname1." อยู่เเล้ว";
+			$text_alert = "..มีการเเจ้งเตือน..\n".$hoonname1." อยู่เเล้ว";
 			$USERID =$row["uid"];
 		}
 		else
