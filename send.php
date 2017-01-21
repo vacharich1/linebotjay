30a1ff30d9510e8f97f24a16cb8f18bdd2065b5a
jay <jr30149_@hotmail.com>
2017-01-22 02:37:18 +0700

    asdasd

--- send.php
+++ send.php
@@ -187,7 +187,7 @@ echo "aaaaa";
 																			[
 																				'type'=> 'uri',
 																				'label'=> 'View detail',
-																				'uri'=> 'http://linebotjay.herokuapp.com/rsi30.php'
+																				'uri'=> 'https://linebotjay.herokuapp.com/rsi30.php'
 																			]
 																											
 																	]
@@ -200,7 +200,7 @@ echo "aaaaa";
 																			[
 																				'type'=> 'uri',
 																				'label'=> 'View detail',
-																				'uri'=> 'http://linebotjay.herokuapp.com/rsielse.php'
+																				'uri'=> 'https://linebotjay.herokuapp.com/rsielse.php'
 																			]
 																											
 																	]
@@ -213,7 +213,7 @@ echo "aaaaa";
 																			[
 																				'type'=> 'uri',
 																				'label'=> 'View detail',
-																				'uri'=> 'http://linebotjay.herokuapp.com/rsi70.php'
+																				'uri'=> 'https://linebotjay.herokuapp.com/rsi70.php'
 																			]
 																											
 																	]
