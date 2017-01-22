3c77eaed63b737b480d480d17c98829aff953a52
jay <jr30149_@hotmail.com>
2017-01-22 18:29:09 +0700

    sdfsdf

--- rsi70.php
+++ rsi70.php
@@ -34,8 +34,12 @@ $sql1 = "SELECT * FROM rsi_70 ORDER BY `price` ASC";
 $result = $link->query($sql1);
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo '<font size="30"'." face='Arial'>";
-		echo $row["hoonname"]."  ".$row["price"].'\r\n';
+		echo "<br>";
+		echo "<font size='50' face='Arial'>";
+		echo "<table>";
+		echo $row["hoonname"]."  :  ".$row["price"];
+		echo "</table>";
+		echo "</font>";
 	}
 
 }
