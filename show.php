824562b5f2e33903bec80976cfabb7daed89668d
jay <jr30149_@hotmail.com>
2017-01-22 11:20:55 +0700

    sdfsdfsf

--- show.php
+++ show.php
@@ -78,10 +78,9 @@ $events = json_decode($content, true);
 							];
 		 
 				$post_data = [
-					"to" => 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b',
+					"to" => $USERID,
 					"messages" => [$messages33]
 				];
-				echo 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b'
 				
 				$header = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 		 
