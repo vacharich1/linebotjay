f3f24dc70a0fe664f54f83c4019dc4dd39a3090d
jay <jr30149_@hotmail.com>
2017-02-19 22:36:01 +0700

    dfgdfg

---
+++ show_chart.php
@@ -0,0 +1,51 @@
+<?php
+
+$host= "sql6.freemysqlhosting.net";
+//$db = "sql6150739";
+$db = "sql6155499";
+$CHAR_SET = "charset=utf8"; 
+ 
+//$username = "sql6150739";    
+//$password = "xiGjqcGnZb";   
+$username = "sql6155499";    
+$password = "xwBrDIuGaA";  
+	
+
+$link = mysqli_connect($host, $username, $password, $db);
+if (!$link) {
+    die('Could not connect: ' . mysqli_connect_errno());
+}
+
+	
+$text_alert=""; 
+$USERID ="";
+	
+
+$link = mysqli_connect($host, $username, $password, $db);
+if (!$link) {
+    	die('Could not connect: ' . mysqli_connect_errno());
+}
+
+
+
+$sql1 = "SELECT * FROM show_all ORDER BY `hoonname` ASC";
+$result = $link->query($sql1);
+$check_rsi_send="";
+
+if ($result->num_rows > 0) {
+	 while($row = $result->fetch_assoc()) {
+		$hoonn=(string)$row["hoonname"];
+		$uuurl = (string)"https://www.botbottest.club/".$textcut[1]."60.jpg";
+		$uuurl2 = (string)"https://www.botbottest.club/".$textcut[1].".day.jpg";
+		$uuurl3 = (string)"https://www.botbottest.club/".$textcut[1].".week.jpg";
+		$uuurl4 = (string)"https://www.botbottest.club/".$textcut[1].".month.jpg";
+		echo "<img src='".$uuurl."' /><br />";
+		echo "<img src='".$uuurl2."' /><br />";
+		echo "<img src='".$uuurl3."' /><br />";
+		echo "<img src='".$uuurl4."' /><br />";
+	 }
+}
+
+
+
+?>
