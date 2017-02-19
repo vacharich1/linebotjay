dfc8f267176f252f895d40359943b3a85a29d6d4
jay <jr30149_@hotmail.com>
2017-02-19 22:01:59 +0700

    sdfsdf

--- bot.php
+++ bot.php
@@ -342,7 +342,7 @@ if (!is_null($events['events'])) {
 								else {
 										echo "Error: " . $sql . "<br>" . mysqli_error($link);
 								}
-								sleep(3);	
+								sleep(4);	
 								$link_pic ="https://www.botbottest.club/".$textcut[1]."".$timeframe_chart.".jpg";
 								$messages33 = [	 'type' => 'template',
 													 'altText' => 'test',
