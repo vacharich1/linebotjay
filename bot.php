3d35fd95eb5698c14557254f4fcc41e435c024ec
jay <jr30149_@hotmail.com>
2017-02-19 21:30:10 +0700

    sdfsdfsd

--- bot.php
+++ bot.php
@@ -341,44 +341,6 @@ if (!is_null($events['events'])) {
 								}
 								sleep(4);	
 								
-								$link_pic ="https://www.botbottest.club/".$textcut[1]."".$timeframe_chart.".jpg";
-								$messages33 = [	 'type' => 'template',
-													 'altText' => 'test',
-													 'template' => [	'type' => 'buttons', 
-																		'thumbnailImageUrl'=> $link_pic,
-																		'title' => 'Doji',
-																		'text'  => 'Doji',
-																		'actions' => [
-																				[
-																					'type'=> 'uri',
-																					'label'=> 'View detail',
-																					'uri'=> $link_pic
-																				]
-																				
-																         ]
-															
-															          ]
-											 
-											 ];
-											 
-									// Make a POST Request to Messaging API to reply to sender
-									$url = 'https://api.line.me/v2/bot/message/reply';
-									$data = [
-										'replyToken' => $replyToken,
-										'messages' => [$messages33]
-									];
-									$post = json_encode($data);
-									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-						
-									$ch = curl_init($url);
-									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-									$result = curl_exec($ch);
-									curl_close($ch);
-									
 						}
 						
 					}
