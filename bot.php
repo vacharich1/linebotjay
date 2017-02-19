e297e0dcfc4c0f294250c5f35c749771f47327c9
jay <jr30149_@hotmail.com>
2017-02-19 21:54:38 +0700

    sdfsdf

--- bot.php
+++ bot.php
@@ -342,25 +342,44 @@ if (!is_null($events['events'])) {
 								else {
 										echo "Error: " . $sql . "<br>" . mysqli_error($link);
 								}
-								sleep(0.3);	
+								sleep(3);	
 								
-								$messages556 = ['type' => 'text','text' => "coming soon..."];
-								$url = 'https://api.line.me/v2/bot/message/reply';
-								$data = [
-											'replyToken' => $replyToken,
-											'messages' => [$messages556]
-										];
-								$post = json_encode($data);
-								$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-									
-								$ch = curl_init($url);
-								curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-								curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-								curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-								curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-								curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-								$result = curl_exec($ch);
-								curl_close($ch);
+								$messages33 = [	 'type' => 'template',
+													 'altText' => 'test',
+													 'template' => [	'type' => 'buttons', 
+																		'thumbnailImageUrl'=> "https://www.botbottest.club/doji.jpg",
+																		'title' => 'Doji',
+																		'text'  => 'Doji',
+																		'actions' => [
+																				[
+																					'type'=> 'uri',
+																					'label'=> 'View detail',
+																					'uri'=> 'http://linebotjay.herokuapp.com/doji.php'
+																				]
+																				
+																         ]
+															
+															          ]
+											 
+											 ];
+											 
+									// Make a POST Request to Messaging API to reply to sender
+									$url = 'https://api.line.me/v2/bot/message/reply';
+									$data = [
+										'replyToken' => $replyToken,
+										'messages' => [$messages33]
+									];
+									$post = json_encode($data);
+									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						
+									$ch = curl_init($url);
+									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									$result = curl_exec($ch);
+									curl_close($ch);
 							}
 							
 						}
