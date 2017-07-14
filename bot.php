36852f6bcc94818ecdf79cc61a22f77a0b280e44
jay <jr30149_@hotmail.com>
2017-07-14 21:28:00 +0700

    sssss

--- bot.php
+++ bot.php
@@ -109,21 +109,22 @@ if (!is_null($events['events'])) {
 							$messages55 = ['type' => 'text','text' => $event['source']['userId']];
 							// Make a POST Request to Messaging API to reply to sender
 							$url = 'https://api.line.me/v2/bot/message/reply';
-							$data = [
-										'replyToken' => $replyToken,
+							
+									$data = [
+										'to' => 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b',
 										'messages' => [$messages55]
 									];
-							$post = json_encode($data);
-							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-									
-							$ch = curl_init($url);
-							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-							$result = curl_exec($ch);
-							curl_close($ch);
+									$post = json_encode($data);
+									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						
+									$ch = curl_init($url);
+									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+									$result = curl_exec($ch);
+									curl_close($ch);
 					}
 				if($text=="doji" || $text=="Doji" || $text=="DOJI" || $text=="DOji" || $text=="DOJi" || $text=="DojI" || $text=="DoJi")
 				{
