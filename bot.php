0fafb8a3ec35006ff50626468334d2feb4d269dd
jay <jr30149_@hotmail.com>
2017-07-14 21:25:02 +0700

    sdfsdfdsf

--- bot.php
+++ bot.php
@@ -103,35 +103,27 @@ if (!is_null($events['events'])) {
 				// Get text sent
 				$text = $event['message']['text'];
 				
-				if($text == '@@@')
-					{		//$event['source']['userId'] == 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b'
+				if($text == '@@@@')
+					{
 							$replyToken = $event['replyToken'];
 							$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
 							// Make a POST Request to Messaging API to reply to sender
-							
-							$post_data = [
-											'to' => 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b',
-											'messages' => [$messages55]
-										 ];
-							
-							
-							// Make a POST Request to Messaging API to reply to sender
-									$url = 'https://api.line.me/v2/bot/message/reply';
-									$data = [
-										'to' => 'Ub5f45b12f0f8f8a3a08e5b52ebbcc96b',
+							$url = 'https://api.line.me/v2/bot/message/reply';
+							$data = [
+										'replyToken' => $replyToken,
 										'messages' => [$messages55]
 									];
-									$post = json_encode($data);
-									$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-						
-									$ch = curl_init($url);
-									curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-									curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-									curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-									curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-									curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-									$result = curl_exec($ch);
-									curl_close($ch);
+							$post = json_encode($data);
+							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+									
+							$ch = curl_init($url);
+							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							$result = curl_exec($ch);
+							curl_close($ch);
 					}
 				if($text=="doji" || $text=="Doji" || $text=="DOJI" || $text=="DOji" || $text=="DOJi" || $text=="DojI" || $text=="DoJi")
 				{
