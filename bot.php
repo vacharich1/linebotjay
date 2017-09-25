fd6b7b4cd7c4e0b942c9098183f02cc4fd8f4713
vacharich1 <jr30149_@hotmail.com>
2017-09-25 09:08:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2389,7 +2389,7 @@ if (!is_null($events['events'])) {
 				if($text == '@@@@')
 					{
 							$replyToken = $event['replyToken'];
-							$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+							$messages55 = ['type' => 'text','text' => $event['source']['userId']];
 							// Make a POST Request to Messaging API to reply to sender
 							$url = 'https://api.line.me/v2/bot/message/reply';
 							$data = [
@@ -3806,7 +3806,7 @@ if (!is_null($events['events'])) {
 					if($text == '@@@@')
 					{
 							$replyToken = $event['replyToken'];
-							$messages55 = ['type' => 'text','text' => $event['source']['groupId']];
+							$messages55 = ['type' => 'text','text' => $event['source']['userId']];
 							// Make a POST Request to Messaging API to reply to sender
 							$url = 'https://api.line.me/v2/bot/message/reply';
 							$data = [
