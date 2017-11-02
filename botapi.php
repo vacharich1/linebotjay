0238b086c593b7bc96d9131e56ef6564923cc24e
jay <jr30149_@hotmail.com>
2017-11-02 23:22:55 +0700

    dfgdfg

--- botapi.php
+++ botapi.php
@@ -504,24 +504,29 @@ if (!is_null($events['events'])) {
 												 'originalContentUrl' => 'https://kumishabu.com/pic/picture2.JPG',
 												 'previewImageUrl' => 'https://kumishabu.com/pic/picture2.JPG'
 									  ];
-						
-						$replyToken = $event['replyToken'];
-						// Make a POST Request to Messaging API to reply to sender
-						$url = 'https://api.line.me/v2/bot/message/reply';
-						$data = [
-									'replyToken' => $replyToken,
+									  
+						$format_text = [
+								"type" => "text",
+								"text" => $text
+							];
+						$post_data = [
+									'to' => $userid,
 									'messages' => [$messages55]
-								];
-						$post = json_encode($data);
-						$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-						$ch = curl_init($url);
-						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+						];
+						$header = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+						echo "ssss";
+					 
+						$ch = curl_init('https://api.line.me/v2/bot/message/push');
+						curl_setopt($ch, CURLOPT_POST, true);
+						curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-						curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-						curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+						curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+					 
 						$result = curl_exec($ch);
 						curl_close($ch);
+						
+						
 					}
 					else
 					{
