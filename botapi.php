cf275d94055f34ab8a0fd14ec79d507e952598fb
jay <jr30149_@hotmail.com>
2017-11-07 00:44:00 +0700

    dfgdfgf

--- botapi.php
+++ botapi.php
@@ -152,9 +152,9 @@ if (!is_null($events['events'])) {
 		if($event['source']['groupId'] == 'C877d5aaeeb523122482b7e0580450cd7')
 		{
 			if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-							$mess = $event['message']['id'];
+							$mess = $event['message']['image'];
 							$replyToken = $event['replyToken'];
-							$messages55 = ['type' => 'text',
+							$messages55 = ['type' => "text",
 										   'text' => $mess
 										   ];
 							// Make a POST Request to Messaging API to reply to sender
